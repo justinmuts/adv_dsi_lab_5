6a9e5ab6596293797309e9529a05ede9fc8abf58
justinmuts <37526487+justinmuts@users.noreply.github.com>
2022-03-19 15:33:50 +1100

    pytorch regression

--- src/models/pytorch.py
+++ src/models/pytorch.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -45,7 +45,7 @@ class PytorchDataset(Dataset):
     to_tensor(data)
         Convert Pandas Series to Pytorch tensor
     """
-        
+    
     def __init__(self, X, y):
         self.X_tensor = self.to_tensor(X)
         self.y_tensor = self.to_tensor(y)
@@ -58,3 +58,164 @@ class PytorchDataset(Dataset):
     
     def to_tensor(self, data):
         return torch.Tensor(np.array(data))
+
+
+
+class PytorchBinary(nn.Module):
+    """
+Create in src/models/pytorch.py a class called PytorchBinary that
+inherits from nn.Module with:
+
+num_features as input parameter
+attributes:
+layer_1: fully-connected layer with 256 neurons
+layer_out: fully-connected layer with 1 neurons
+sigmoid: sigmoid function
+methods:
+forward() with inputs as input parameter, perform ReLU and DropOut
+on the fully-connected layer followed by the output layer with sigmoid
+
+    """
+    def __init__(self, num_features):
+        super(PytorchBinary, self).__init__()
+        
+        self.layer_1 = nn.Linear(num_features, 256)
+        self.layer_out = nn.Linear(256, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = F.dropout(F.relu(self.layer_1(x)))
+        x = self.sigmoid(self.layer_out(x))
+        return (x)
+
+
+def train_binary(train_data, model, criterion, optimizer, batch_size, device, scheduler=None, generate_batch=None):
+    """Train a Pytorch binary classification model
+
+    Parameters
+    ----------
+    train_data : torch.utils.data.Dataset
+        Pytorch dataset
+    model: torch.nn.Module
+        Pytorch Model
+    criterion: function
+        Loss function
+    optimizer: torch.optim
+        Optimizer
+    bacth_size : int
+        Number of observations per batch
+    device : str
+        Name of the device used for the model
+    scheduler : torch.optim.lr_scheduler
+        Pytorch Scheduler used for updating learning rate
+    collate_fn : function
+        Function defining required pre-processing steps
+
+    Returns
+    -------
+    Float
+        Loss score
+    Float:
+        Accuracy Score
+    """
+    
+    # Set model to training mode
+    model.train()
+    train_loss = 0
+    train_acc = 0
+    
+    # Create data loader
+    data = DataLoader(train_data, batch_size=batch_size, shuffle=True, collate_fn=generate_batch)
+    
+    # Iterate through data by batch of observations
+    for feature, target_class in data:
+
+        # Reset gradients
+        optimizer.zero_grad()
+        
+        # Load data to specified device
+        feature, target_class = feature.to(device), target_class.to(device).to(torch.float32)
+        
+        # Make predictions
+        output = model(feature)
+        
+        # Calculate loss for given batch
+        loss = criterion(output, target_class.unsqueeze(1))
+        
+        # Calculate global loss
+        train_loss += loss.item()
+        
+        # Calculate gradients
+        loss.backward()
+
+        # Update Weights
+        optimizer.step()
+        
+        # Calculate global accuracy
+        train_acc += (output.argmax(1) == target_class).sum().item()
+
+    # Adjust the learning rate
+    if scheduler:
+        scheduler.step()
+
+    return train_loss / len(train_data), train_acc / len(train_data)
+
+
+
+def test_binary(test_data, model, criterion, batch_size, device, generate_batch=None):
+    """Calculate performance of a Pytorch binary classification model
+
+    Parameters
+    ----------
+    test_data : torch.utils.data.Dataset
+        Pytorch dataset
+    model: torch.nn.Module
+        Pytorch Model
+    criterion: function
+        Loss function
+    bacth_size : int
+        Number of observations per batch
+    device : str
+        Name of the device used for the model
+    collate_fn : function
+        Function defining required pre-processing steps
+
+    Returns
+    -------
+    Float
+        Loss score
+    Float:
+        Accuracy Score
+    """    
+    
+    # Set model to evaluation mode
+    model.eval()
+    test_loss = 0
+    test_acc = 0
+    
+    # Create data loader
+    data = DataLoader(test_data, batch_size=batch_size, collate_fn=generate_batch)
+    
+    # Iterate through data by batch of observations
+    for feature, target_class in data:
+        
+        # Load data to specified device
+        feature, target_class = feature.to(device), target_class.to(device).to(torch.float32)
+        
+        # Set no update to gradients
+        with torch.no_grad():
+            
+            # Make predictions
+            output = model(feature)
+            
+            # Calculate loss for given batch
+            loss = criterion(output, target_class.unsqueeze(1))
+
+            # Calculate global loss
+            test_loss += loss.item()
+            
+            # Calculate global accuracy
+            test_acc += (output.argmax(1) == target_class).sum().item()
+
+    return test_loss / len(test_data), test_acc / len(test_data)
+
